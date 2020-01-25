d53b5c7c16fc9f21c1d1ff22357a3c8e4962980b
AdamUArk <jahughes@uark.edu>
2020-01-24 23:51:21 -0600

    hidden sheets make need for specifying the sheet name

--- 0-Import.R
+++ 0-Import.R
@@ -4,7 +4,7 @@ download.file(url, destfile = "t2016.xlsx",  mode = "wb")
 
 library(readxl)
 
-kansas_state <- read_xlsx("t2016.xlsx", skip = 165, col_names = FALSE)
+kansas_state <- read_xlsx("t2016.xlsx", skip = 165, col_names = FALSE, sheet = 'Totals')
 
 gender <- kansas_state[1:4]
 names(gender) <- c("COLLEGE", "N", "Male", "F")
