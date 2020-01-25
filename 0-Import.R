f70a51028d2dd7f71fbadec29e36b1d42fb44caa
AdamUArk <jahughes@uark.edu>
2020-01-24 23:08:02 -0600

    make simple for stackoverflow example

--- 0-Import.R
+++ 0-Import.R
@@ -8,6 +8,3 @@ kansas_state <- read_xlsx("t2016.xlsx", skip = 165, col_names = FALSE)
 
 gender <- kansas_state[1:4]
 names(gender) <- c("COLLEGE", "N", "Male", "F")
-
-tenure <- cbind(kansas_state[1], kansas_state[16:18])
-names(tenure) <- c("COLLEGE", "TENURE", "TENURE TRACK", "NON TENURE")
